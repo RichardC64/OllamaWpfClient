822a344c941e5f613bbe4e3f14c43694b82bceae
Richard Clark <rclark@c2i.fr>
2024-05-23 08:00:21 +0200

    Ajoutez des fichiers projet.

---
+++ ConversationItem.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.ObjectModel;
+using CommunityToolkit.Mvvm.ComponentModel;
+
+namespace WpfApp1;
+
+[ObservableObject]
+public partial class MainWindowViewModel
+{
+    [ObservableProperty]
+    private ObservableCollection<ConversationItem> _conversationItems = new();
+}
+
+public record ConversationItem(string Text, ConversationSource Source);
+
+public enum ConversationSource
+{
+    User,
+    Bot
+}
